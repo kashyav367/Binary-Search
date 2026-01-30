1e611ec46e8b95a011597ed1e5715905e94fe7aa
Ankit kumar Singh <ankit12030621028@gmail.com>
2026-01-31 00:23:21 +0530

    5th

---
+++ find peak Element.js
@@ -0,0 +1,20 @@
+function peakElement(arr){
+    let l = 0;
+    let r = arr.length-1;
+
+    while(l<r){
+        let m = l+Math.floor((r-l)/2);
+
+        if(arr[m] < arr[m+1]){
+            l = m+1;
+        }
+        else{
+            r=m;
+        }
+    }
+    return r;
+  }
+
+   let arr = [1,2,3,1];
+
+    console.log(peakElement(arr));
