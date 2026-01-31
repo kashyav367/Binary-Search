1aff2a4fb1960bb62c4a5d44ebc2df693b6be28c
Ankit kumar Singh <ankit12030621028@gmail.com>
2026-02-01 02:31:10 +0530

    implement binary search to find first and last position of an element in a sorted array

---
+++ find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,36 @@
+function searchRange(arr,target){
+    let l = 0;
+    let r = arr.length-1;
+    let ans = [-1,-1];
+
+    while(l < r){
+        let m = l+Math.floor((r-l)/2);
+        if(arr[m] < target){
+            l = m+1;
+        }
+        else {
+            r = m;
+        }
+    }
+    if(arr[l] === target) ans[0] = l;
+
+      l = 0;
+      r = arr.length-1;
+
+    while(l < r){
+        let m = l+Math.ceil((r-l)/2);
+        if(arr[m] > target){
+            r = m-1;
+        }
+        else {
+            l = m;
+        }
+    }
+    if(arr[l] === target) ans[1] = l;
+    return ans;
+}
+
+let arr = [5,7,7,8,8,10]
+let target = 8;
+
+console.log(searchRange(arr,target));
